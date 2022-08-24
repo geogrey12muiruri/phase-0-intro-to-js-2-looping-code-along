d2de9888ac632c147ee4b07493d34d96b7c6bb57
geogrey12muiruri <greggambugua@gmail.com>
2022-08-25 00:43:58 +0300

    completed the assaignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards(names, event) {
+    let messages = [];
+    for(let i=0; i<names.length; i++) {
+        messages.push("Thank you, " + names[i] + ","  + " for the wonderful " + event + " gift!");
+    }
+    return messages;
+    
+}
+function countDown() {
+    let countdown =11;
+    while (countdown > 0) {
+        countdown--;
+        console.log(countdown)
+
+    }
+    
+
+}
+
